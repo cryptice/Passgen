9e7bc3823b03b86211339b6ebd2c9dfd01dc0765
Erik Lindblad <erik.lindblad@metaboli.com>
2009-05-02 01:08:08 +0200

    A few parameters implemented.

--- lib/passgen.rb
+++ lib/passgen.rb
@@ -1,6 +1,25 @@
 module Passgen
 
+  DEFAULT_PARAMS = {
+    :length => 10,
+    :lowercase => true,
+    :uppercase => true,
+    :digits => true,
+    :symbols => false
+  }
+
   def self.generate(params={})
-    rand(100000)
+    options = DEFAULT_PARAMS.merge(params)
+    p params
+
+    valid_tokens = []
+    
+    valid_tokens += ("a".."z").to_a if options[:lowercase]
+    valid_tokens += ("A".."Z").to_a if options[:uppercase]
+    valid_tokens += ("0".."9").to_a if options[:digits]
+    valid_tokens += %w{! @ # £ $ % & / ( ) + ? *} if options[:symbols]
+    puts valid_tokens.join
+    
+    Array.new(options[:length]) {valid_tokens[rand(valid_tokens.size)]}.join
   end
 end
--- spec/passgen_spec.rb
+++ spec/passgen_spec.rb
@@ -5,12 +5,61 @@ describe "Using passgen" do
   before do
     srand(1)
   end
-
+  
   it "should return password with default settings." do
-    Passgen::generate.should eql("h")
+    Passgen::generate.should eql("LRmijlfpaq")
   end
 
-  it "should return capitals only password" do
-    Passgen::generate.should eql("h")
+  it "should return password with uppercase chars only" do
+    Passgen::generate(:lowercase => false,
+                      :digits => false).should eql("FLMIJLFPAQ")
   end
+  
+  it "should return password with lowercase chars only" do
+    Passgen::generate(:uppercase => false,
+                      :digits => false).should eql("flmijlfpaq")
+  end
+
+  it "should return password with digits only" do
+    Passgen::generate(:lowercase => false,
+                      :uppercase => false).should eql("5895001769")
+  end
+
+  it "should return password with symbols only" do
+    Passgen::generate(:lowercase => false,
+                      :uppercase => false,
+                      :digits => false,
+                      :symbols => true).should eql("%?*()?%!!@")
+  end
+
+  it "should return password with lowercase and uppercase chars only"
+
+  it "should return password with lowercase and digit chars only"
+
+  it "should return password with lowercase and symbol chars only"
+
+  it "should return password with uppercase and digit chars only"
+
+  it "should return password with uppercase and symbol chars only"
+
+  it "should return password with digit and symbol chars only"
+
+  it "should return password with lowercase, uppercase and digit chars only"
+
+  it "should return password with lowercase, uppercase and symbol chars only"
+
+  it "should return password with lowercase, digit and symbol chars only"
+
+  it "should return password with uppercase, digit and symbol chars only"
+
+  it "should return given number of passwords in an Array"
+
+  it "should return a password with given length"
+
+  it "should return several passwords of variable length"
+
+  it "should use given seed"
+
+  it "should set seed to Time.now + Process.id"
+  
 end
